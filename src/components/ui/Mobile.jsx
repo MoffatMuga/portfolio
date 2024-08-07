f3850ad261806e61b4bda171ea1fe3b424be7f1d
MoffatMuga <moffat.muga@gmail.com>
2024-08-07 15:28:22 +0300

    add ui

--- src/components/ui/Mobile.jsx
+++ src/components/ui/Mobile.jsx
@@ -1,7 +1,7 @@
-import {Sheet, SheetContent, SheetTrigger} from './sheet'
+import { Sheet, SheetContent, SheetTrigger } from './sheet'
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
-import {CiMenuFries} from 'react-icons/ci'
+import { CiMenuFries } from 'react-icons/ci'
 import { FaLocationArrow } from "react-icons/fa6";
 import { Button } from './button';
 
@@ -18,6 +18,10 @@ const links = [
         name: "projects",
         path: "/projects"
     },
+    {
+        name: "contact",
+        path: "/contact"
+    },
 ]
 
 export default function Mobile() {
@@ -32,36 +36,36 @@ export default function Mobile() {
     }
 
     const pathname = usePathname();
-  return (
-   <Sheet >
-    <SheetTrigger className='flex justify-center items-center'>
-        <CiMenuFries className='text-[32px] text-main'/>
-    </SheetTrigger>
-    <SheetContent className='flex flex-col  border-l-2 border-l-main  '>
-        <div className='mt-32  items-center  text-center'>
-       
-            <Link href='/' className='text-3xl'>
-                Dev<span className='text-main '>Muga</span>
-            </Link> 
+    return (
+        <Sheet >
+            <SheetTrigger className='flex justify-center items-center'>
+                <CiMenuFries className='text-[32px] text-main' />
+            </SheetTrigger>
+            <SheetContent className='flex flex-col  border-l-2 border-l-main  '>
+                <div className='mt-32  items-center  text-center'>
 
-            <div className='space-y-8 flex mt-10 flex-col'>
+                    <Link href='/' className='text-3xl'>
+                        Dev<span className='text-main '>Muga</span>
+                    </Link>
 
-                    {links.map((item, index) => (
+                    <div className='space-y-8 flex mt-10 flex-col'>
 
-                        <Link key={index} href={item.path} className={`${item.path === pathname && "text-main border-main border-b-2"} capitalize font-medium hover:text-main transition-all`}>
-                            {item.name}
-                        </Link>
+                        {links.map((item, index) => (
 
-                    ))}
-                    <Button className='bg-transparent border border-main gap-1' onClick={handleContactClick}>
-                        contact
-                        <span className='text-main text-2xl'>
-                            <FaLocationArrow />
-                        </span>
-                    </Button>
+                            <Link key={index} href={item.path} className={`${item.path === pathname && "text-main border-main border-b-2"} capitalize font-medium hover:text-main transition-all`}>
+                                {item.name}
+                            </Link>
+
+                        ))}
+                        <Button className='bg-transparent border border-main gap-1' onClick={handleContactClick}>
+                            Chat Me
+                            <span className='text-main text-2xl'>
+                                <FaLocationArrow />
+                            </span>
+                        </Button>
+                    </div>
                 </div>
-        </div>
-    </SheetContent>
-   </Sheet>
-  )
+            </SheetContent>
+        </Sheet>
+    )
 }
